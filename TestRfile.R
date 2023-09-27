f6228ccb8c63409b26912667e4cde428624f95dc
catvee <115720964+catvee@users.noreply.github.com>
2023-09-27 13:24:02 +0200

    R File uploaded

---
+++ TestRfile.R
@@ -0,0 +1,23 @@
+#' ---
+#' title: "Analysis"
+#' author: "CV"
+#' ---
+
+#' Script for testing
+
+rm(list=ls()) # clear environment
+
+#' # 1 Import libraries, functions and data
+# 1 Import libraries, functions and data----
+library(dplyr)
+library(gridExtra)
+
+var2     <- 2
+
+#' Functions loaded through the **evaluation functions** script  
+#' **f.getData**: takes as argument a dataset and transforms it 
+#' **Plotting functions**: The plotting functions get as first argument the dataframe containing the data to plot.
+#'                    After this different arguments follow, such as the x and y variable, a variable for coloring
+#'                    and a plot title.  
+
+
